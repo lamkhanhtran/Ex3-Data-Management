eb01d7224baca0517cf3e8d96c71b1a305ba1baf
lamkhanhtran <tranphamlamkhanh@gmail.com>
2024-06-29 18:29:59 +0900

    HoT: initial revision

---
+++ HeadOrTail.py
@@ -0,0 +1,13 @@
+import random
+
+print(f"Tossing a coin...")
+coinHit = [0, 0]
+for i in range(3):
+    print(f"Round {i}: ", end="")
+    ans = random.randrange(0, 1)
+    coinHit[ ans ] += 1
+    if ans == 0:
+        print(f"Heads")
+    else:
+        print(f"Tails")
+print(f"Heads: {coinHit[ 0 ]}, Tails: {coinHit[ 1 ]}")
