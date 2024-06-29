e52bff34b91d09d917c45a6a5f03c0880ffe2e2b
lamkhanhtran <tranphamlamkhanh@gmail.com>
2024-06-29 19:35:17 +0900

    HoT: initial revision

--- HeadOrTail.py
+++ HeadOrTail.py
@@ -4,7 +4,7 @@ print(f"Tossing a coin...")
 coinHit = [0, 0]
 for i in range(3):
     print(f"Round {i}: ", end="")
-    ans = random.randrange(0, 1)
+    ans = random.randrange(0, 2)
     coinHit[ ans ] += 1
     if ans == 0:
         print(f"Heads")
