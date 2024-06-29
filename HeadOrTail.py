fcabc471c546bf451bd81b8dd68244c37ff8383f
lamkhanhtran <tranphamlamkhanh@gmail.com>
2024-06-29 19:54:50 +0900

    User name added

--- HeadOrTail.py
+++ HeadOrTail.py
@@ -1,10 +1,13 @@
 import random
 
+print(f"Who are you?")
+name = input()
+print(f"Hello, {name}!")
 print(f"Tossing a coin...")
 coinHit = [0, 0]
 for i in range(3):
     print(f"Round {i}: ", end="")
-    ans = random.randrange(0, 1)
+    ans = random.randrange(0, 2)
     coinHit[ ans ] += 1
     if ans == 0:
         print(f"Heads")
